fef672a057528a1a5381fce989dcf7b729eb7c28
John Doe <randomm200@mail.ru>
2021-07-26 21:27:17 +0300

    fix name #4 №3.1

--- src/mvc/observer/interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IObserver {
-  observers: Function[]
-
-  addObserver(observer: Function): void
-  removeObserver(observer: Function): void
-  callAllObserver(obj?:{[key:string]:number}): void
-}
-
